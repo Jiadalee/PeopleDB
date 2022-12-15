fb894679f06751abf5c5941b43aa6b410bad0bed
jiadali <Jiada.Li@colostate.edu>
2022-12-14 21:37:15 -0700

    add peopleDB project

---
+++ src/main/java/com/neutrinosys/peopledb/repository/PeopleRepository.java
@@ -0,0 +1,10 @@
+package com.neutrinosys.peopledb.repository;
+
+import com.neutrinosys.peopledb.model.Person;
+
+public class PeopleRepository {
+    public Person save(Person person) {
+
+        return person;
+    }
+}
