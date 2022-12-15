fb7786c048bda6e77998df8e88d6cbe2e7f3edbb
jiadali <Jiada.Li@colostate.edu>
2022-12-14 22:00:49 -0700

    add: JDBC connection test

--- src/test/java/com/neutrinosys/peopledb/repository/PeopleRepositoryTests.java
+++ src/test/java/com/neutrinosys/peopledb/repository/PeopleRepositoryTests.java
@@ -3,6 +3,9 @@ package com.neutrinosys.peopledb.repository;
 import com.neutrinosys.peopledb.model.Person;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
@@ -11,10 +14,22 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class PeopleRepositoryTests {
 
     @Test
-    public void canSave(){
+    public void canSaveOnePerson() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:h2:~/peopletest".replace("~", System.getProperty("user.home")));
         PeopleRepository repo = new PeopleRepository();
         Person john  = new Person("John", "Smith", ZonedDateTime.of(1980, 11, 15, 15, 15, 0, 0, ZoneId.of("-6")));
         Person savedPerson =  repo.save(john);
         assertThat(savedPerson.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void canSaveTwoPeople(){
+        PeopleRepository repo = new PeopleRepository();
+        Person john  = new Person("John", "Smith", ZonedDateTime.of(1980, 11, 15, 15, 15, 0, 0, ZoneId.of("-6")));
+        Person bobby  = new Person("Bobby", "Smith", ZonedDateTime.of(1970, 9, 15, 10, 11, 0, 0, ZoneId.of("-6")));
+        Person savedPerson1 =  repo.save(john);
+        Person savedPerson2 =  repo.save(bobby);
+        assertThat(savedPerson1.getId()).isNotEqualTo(savedPerson2.getId());
+        //assertThat(savedPerson1.getId()).isGreaterThan(0);
     }
 }
